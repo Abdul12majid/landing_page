24710f073d85bc59d7f198d55ba4b461594605b1
Abdul12majid <yisaabdulmajid@gmail.com>
2023-09-22 03:42:38 +0100

    initial commit

---
+++ ben_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BenAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ben_app'
---
+++ ben_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+	
+	if request.method=='POST':
+		name=request.POST['name']
+		email=request.POST['email']
+		message=request.POST['comment']
+		messages.success(request, (f'Hi {name}, your message was received, kindly await reply.'))
+		return render(request, 'index2.html', {'name':name})
+	return render(request, 'index2.html', {})
